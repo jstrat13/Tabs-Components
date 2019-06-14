9f128995bdfbff5837e1691cc71340c257bef4cf
jstrat13 <49758720+jstrat13@users.noreply.github.com>
2019-06-13 21:25:11 -0500

    wrote code for adding and removing class names from links

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -24,20 +24,22 @@ class TabLink {
     const links = document.querySelectorAll('.tabs-link');
 
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
-    // Array.from(links).forEach();
+    links.forEach( (item) => {
+     item.classList.remove('tabs-link-selected');
+    });
 
     // Add a class named "tabs-link-selected" to this link
-    // this.element;
+    this.element.classList.add('tabs-link-selected');
     
     // Call the select method on the item associated with this link
-
+      this.tabItem.select()
   }
 }
 
 class TabItem {
   constructor(element) {
     // Assign this.element to the passed in element
-    // this.element;
+    this.element = element;
   }
 
   select() {
